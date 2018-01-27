be05ac52d5d07965c7c316982fc36c516788f077
mayurshedage <shedage.mayur@gmail.com>
2018-01-27 13:57:06 +0530

    javascript fetch method example

---
+++ Examples/sample.php
@@ -0,0 +1,109 @@
+<?php
+
+$response = array();
+
+if (!empty($_REQUEST)) {
+	$response['success'] = 1;
+	$response['data'] = $_REQUEST;
+	echo json_encode($response);
+	exit();
+}else{
+	$response['success'] = 0;
+	$response['message'] = 'Empty request.';
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JavaScript</title>
+</head>
+<body>
+	<button id="cc_install">Test</button>
+</body>
+<script type="text/javascript">
+	document.addEventListener("DOMContentLoaded", function(event) {
+
+		let queryString = (obj) => {
+			return Object.keys(obj).reduce(
+				function(a,k){
+					a.push(k+'='+encodeURIComponent(obj[k]));
+					return a
+				},[]).join('&');
+		};
+
+		let cc_display = (id, value) => {
+			if(typeof(id) === undefined || id == ''){
+				return;
+			}
+			document.getElementById(id).style.display = value;
+		};
+
+		let cc_fadeIn = (id, callback) => {
+			if(typeof(id) === undefined || id == ''){
+				return;
+			}
+			document.getElementById(id).style.opacity = 1;
+			document.getElementById(id).style.display = 'block';
+			document.getElementById(id).style.transition = 'opacity 1s';
+			if(callback){
+				callback();
+			}
+		};
+
+		let cc_fadeOut = (id, callback) => {
+			if(typeof(id) === undefined || id == ''){
+				return;
+			}
+			document.getElementById(id).style.opacity = 0;
+			if(callback){
+				callback();
+			}
+		};
+
+		let cc_addEventLicener = (id, event, callback) => {
+			if(typeof(id) === undefined || id == ''){
+				return;
+			}
+			var event = event || 'click';
+			var callback = callback || function(){};
+			var checkidindom = document.getElementById(id);
+
+			if(typeof(checkidindom) !== undefined && checkidindom !== null){
+				checkidindom.addEventListener(event, callback);
+			}
+		};
+
+		let js_Ajax = () => {
+			let data = {
+				id: 11,
+			    name: 'Sara',
+			    color: 'red'
+			},
+			ts = Math.floor(Date.now() / 1000);
+			var request = new Request('sample.php?ts='+ts, {
+				method: 'post',
+				headers: {
+					"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+				},
+				body: queryString(data)
+			});
+
+			fetch(request).then(res => res.json()).then(
+				function (data) {
+					console.log('JSON response', data);
+					if(typeof(data) !== undefined && data.hasOwnProperty('data') && data.data.hasOwnProperty('name')){
+						console.log('name is ',data.data['name']);
+					}
+				}
+			).catch(
+				function (error) {
+					console.log('Request failed', error);
+				}
+			);
+		};
+
+		cc_addEventLicener('cc_install', 'click', js_Ajax);
+	});
+</script>
+</html>
