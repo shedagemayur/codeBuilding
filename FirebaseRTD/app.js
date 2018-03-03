dacc6a80a2380ba74a45ab98f9baf4369ceffd7e
mayurshedage <shedage.mayur@gmail.com>
2018-03-03 13:12:30 +0530

    added example of firebase realtime database

---
+++ FirebaseRTD/app.js
@@ -0,0 +1,37 @@
+(function(){
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyCEzhJUx1ot75fAAe9Ww2yOskVSgdtTWd8",
+		authDomain: "cometchat-6f734.firebaseapp.com",
+		databaseURL: "https://cometchat-6f734.firebaseio.com",
+		projectId: "cometchat-6f734",
+		storageBucket: "cometchat-6f734.appspot.com",
+		messagingSenderId: "68798032406"
+	};
+	firebase.initializeApp(config);
+
+	// Get elements
+	const userInfo = document.getElementById('userinfo');
+
+	// Create database reference
+	const dbRef = firebase.database().ref().child('userinfo');
+
+	// Sync firebase object changes
+	dbRef.on('value', snapshot => {
+		userinfo.innerText = JSON.stringify(snapshot.val(), null, 3);
+	});
+
+	// Set value to database
+	userid = 11;
+	const random = document.getElementById('random');
+	if(random){
+		random.addEventListener('click', function(){
+			userid = Math.floor(Math.random(10, 99)*100);
+			dbRef.set({
+				location: 'India',
+				userid: userid,
+				username: 'Qwerty'
+			});
+		});
+	}
+}());
---
+++ FirebaseRTD/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Firebase Realtime Database</title>
+	<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
+</head>
+<body>
+	<button id="random">Generate ID</button>
+	<pre id="userinfo"></pre>
+	<script type="text/javascript" src="app.js"></script>
+</body>
+</html>
