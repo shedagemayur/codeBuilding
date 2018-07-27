0a10e7d8cdfdc420ba3404f23499d25bd048351c
mayurshedage <mayur.shedage@inscripts.in>
2018-07-27 20:18:24 +0530

    implemented factorial in using promises

--- BasicProgramming/factorialofnumber.js
+++ BasicProgramming/factorialofnumber.js
@@ -7,5 +7,30 @@ var code = (function($){
                 return n * arguments.callee(n - 1);
             };
         }
-	}	 
+	}
 })(code);
+
+let code = (function(){
+	let getFactorial = n =>{
+		return new Promise((resolve,reject)=>{
+			if(n<=1){
+				resolve(1);
+			}
+			resolve(
+				getFactorial(n-1).then(fact => {
+					return fact * n;
+				})
+			)
+		});
+	}
+	return {
+		factorial: function(number){
+			getFactorial(number).then(
+				response => console.log(response)
+			)
+		}
+	}
+})();
+code.factorial(5);
+code.factorial(6);
+code.factorial(7);
